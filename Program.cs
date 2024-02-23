3322134e6de3474f23b24875d2c0036b09a7f23d
Oleg Teterin <teterinov@mail.ru>
2024-02-23 20:08:35 +0300

    I'am won the settings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-console.WriteLine("I'am won the settings");
+Console.WriteLine("I'am won the settings");
 
